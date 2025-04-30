bbcd2df2f2a90f0c78676fcb0374daed6e05a6db
Anand <mobdev2-gepnic@supportgov.in>
2025-04-30 10:09:21 +0530

    Refactor login endpoint to use async database operations and update user retrieval logic

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -3,29 +3,27 @@
 from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
 
 from app.db import get_db
-from app.users.User import User
+from app.users.models import ExpoLogin
 from app.auth.auth import create_access_token, hash_password, verify_password
 
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 router = APIRouter(prefix="/api/auth")
 
-# Mock user database (Replace with real DB)
-fake_users_db = {
-    "admin": {"username": "admin", "password": hash_password("password123")}
-}
-
 @router.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(),db: AsyncSession = Depends(get_db)):
     """Authenticate user and return JWT token"""
-    user = db.query(User).filter(User.username == form_data.username).first()
+    result = await db.execute(
+        select(ExpoLogin).where(ExpoLogin.loginid == form_data.username)
+    )
+    user = result.scalar_one_or_none()
     
-    # user = fake_users_db.get(form_data.username)
     if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
-    access_token = create_access_token({"sub": form_data.username,"role":user.role}, timedelta(minutes=30))
+    access_token = create_access_token({"sub": form_data.username,"role":user.usertype}, timedelta(minutes=30))
     return {"access_token": access_token, "token_type": "bearer"}
